d62c3b371421a5e3c0990d25406e913d13b3ba54
Andryev Lemes <andryev16@gmail.com>
2022-08-17 17:25:55 +0100

    project with orm panache

--- src/main/java/br/com/letscode/domain/repository/CarRepository.java
+++ src/main/java/br/com/letscode/domain/repository/CarRepository.java
@@ -1,13 +1,19 @@
 package br.com.letscode.domain.repository;
 
 import br.com.letscode.domain.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
+@ApplicationScoped
+public class CarRepository implements PanacheRepository<Car> {
 
-    public List<Car> findByColor(String color);
+    public List<Car> findByColor(String color) {
+        return list("color", color);
+    }
 
-    public List<Car> findByModel(String model);
+    public List<Car> findByModel(String model) {
+        return list("model" , model);
+    }
 }
--- src/main/java/br/com/letscode/domain/service/CarService.java
+++ src/main/java/br/com/letscode/domain/service/CarService.java
@@ -3,10 +3,9 @@ package br.com.letscode.domain.service;
 
 import br.com.letscode.domain.model.Car;
 import br.com.letscode.domain.repository.CarRepository;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -16,12 +15,13 @@ public class CarService {
 
     private final CarRepository carRepository;
 
+    @Inject
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
     }
 
     public Iterable<Car> findAll() {
-        return  this.carRepository.findAll();
+        return  this.carRepository.listAll();
     }
 
     public void delete(Long id) {
@@ -32,7 +32,8 @@ public class CarService {
         Car car = new Car();
         car.setColor(color);
         car.setModel(model);
-        return this.carRepository.save(car);
+        this.carRepository.persist(car);
+        return car;
     }
 
     public List<Car> findByCor(String color) {
@@ -44,7 +45,7 @@ public class CarService {
     }
 
     public Car findById(Long id) {
-        return this.carRepository.findById(id).get();
+        return this.carRepository.findById(id);
     }
 
 }
